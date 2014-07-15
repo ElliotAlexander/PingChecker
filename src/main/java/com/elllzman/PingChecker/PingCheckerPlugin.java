074bfded7757fcb64e9a1d8954c2bfdf3e7d3fc4
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-15 15:03:16 +0100

    make sure the sender is actually a player before casting

--- src/main/java/com/elllzman/PingChecker/PingCheckerPlugin.java
+++ src/main/java/com/elllzman/PingChecker/PingCheckerPlugin.java
@@ -1,5 +1,6 @@
 package com.elllzman.PingChecker;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -10,10 +11,12 @@ public class PingCheckerPlugin extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
         if(cmd.getName().equalsIgnoreCase("ping")) {
+            if(!(sender instanceof Player)) {
+                sender.sendMessage(ChatColor.RED + "This command can only be called by a player");
+                return true;
+            }
 
-            final Player p = (Player) sender;
-
-            getServer().getScheduler().runTaskAsynchronously(this, new PingChecker(p));
+            getServer().getScheduler().runTaskAsynchronously(this, new PingChecker((Player) sender));
             return true;
         }
         return false;
