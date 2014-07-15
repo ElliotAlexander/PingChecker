07f188d9718cde4ff41c4e25cd67c0829b9a0d70
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-15 14:56:25 +0100

    remove comment and unused import

--- src/main/java/com/elllzman/PingChecker/PingChecker.java
+++ src/main/java/com/elllzman/PingChecker/PingChecker.java
@@ -7,14 +7,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitScheduler;
 
 import java.net.InetAddress;
 import java.util.Date;
 
-/**
- * Created by Elliot on 15/07/2014.
- */
 public class PingChecker extends JavaPlugin {
 
     private Plugin plugin;
